ab894e48ccc98ad60ae39428abd71570fe9b8dcd
GENIUS501 <39778070+GENIUS501@users.noreply.github.com>
2018-11-17 13:21:35 -0600

    17/11/2018 13:21

--- SIGVA-DEMO/Entidades/Ent_Funcionarios.cs
+++ SIGVA-DEMO/Entidades/Ent_Funcionarios.cs
@@ -8,6 +8,53 @@ namespace Entidades
 {
     public class Ent_Funcionarios
     {
+        #region "Propiedades"
+        public string Nombre { get; set; }
+        public string Apellido1 { get; set; }
+        public string Apellido2 { get; set; }
+        public Int32 Id_Puesto {get;set;}
+        public Int32 Id_Departamento {get;set;}
+        public DateTime Fecha_De_Ingreso {get;set;}
+        public string Direccion {get;set;}
+        public Int32 Telefono {get;set;}
+        public string Email {get;set;}
+        public DateTime Fecha_de_Anualidad {get;set;}
+        public string Estatus { get; set; }
+        public Int32 Anos_Institucion_anterior { get; set; }
+        #endregion
 
+        #region "Constructor sin parametros"
+        public Ent_Funcionarios()
+        {
+            Nombre = "";
+            Apellido1 = "";
+            Apellido2 = "";
+            Id_Puesto = 0;
+            Id_Departamento = 0;
+            Direccion = "";
+            Telefono = 0;
+            Email = "";
+            Estatus = "";
+            Anos_Institucion_anterior = 0;
+        }
+        #endregion
+
+        #region "Constructor con parametros"
+        public Ent_Funcionarios(string nombre,string apellido1,string apellido2,Int32 id_puesto,Int32 id_departamento,DateTime fecha_ingreso,string direccion,Int32 telefono,string email,DateTime fecha_anualidad,string estatus,Int32 anos_institucion_anterior)
+        {
+            Nombre = nombre;
+            Apellido1 = apellido1;
+            Apellido2 = Apellido2;
+            Id_Puesto = id_puesto;
+            Id_Departamento = id_departamento;
+            Fecha_De_Ingreso = fecha_ingreso;
+            Direccion = direccion;
+            Telefono = telefono;
+            Email = email;
+            Fecha_de_Anualidad = fecha_anualidad;
+            Estatus = estatus;
+            Anos_Institucion_anterior = anos_institucion_anterior;
+        }
+        #endregion
     }
 }
