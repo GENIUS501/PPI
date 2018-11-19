41863e418cc3957c50976de7f7eb1820900c22e0
GENIUS501 <39778070+GENIUS501@users.noreply.github.com>
2018-11-19 13:22:23 -0600

    19/11/2018 13:22

---
+++ SIGVA-DEMO/Negocios/Neg_Funcionarios.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using AccesoDatos;
+
+
+namespace Negocios
+{
+    public class Neg_Funcionarios
+    {
+        #region "Insertar"
+        public Int32 Insertar(Ent_Funcionarios Data)
+        {
+            Int32 FilasAfectadas = 0;
+            AccesoDatos.AD_Funcionarios IControlador;
+
+            try
+            {
+                IControlador = new AccesoDatos.AD_Funcionarios();
+                FilasAfectadas = IControlador.Insertar(Data);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message, ex);
+            }
+
+            return FilasAfectadas;
+        }
+        #endregion
+    }
+}
