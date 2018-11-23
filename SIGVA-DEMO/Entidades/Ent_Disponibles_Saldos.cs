8bd594e1471ca01211483381b91434c25b5660c2
GENIUS501 <39778070+GENIUS501@users.noreply.github.com>
2018-11-23 00:53:36 -0600

    23/11/2018 0:53

---
+++ SIGVA-DEMO/Entidades/Ent_Disponibles_Saldos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Ent_Disponibles_Saldos
+    {
+        #region "Propiedades"
+        public Int32 Codigo_Reporte { get; set; }
+        public Int32 Ano { get; set; }
+        public Decimal Cantidad_Dias { get; set; }
+        public Int32 Cedula { get; set; }
+        #endregion
+
+        #region "Constructor sin parametros"
+        public Ent_Disponibles_Saldos()
+        {
+            Codigo_Reporte = 0;
+            Ano = 0;
+            Cantidad_Dias = 0;
+            Cedula = 0;
+        }
+        #endregion
+
+        #region "Constructor con parametros"
+        public Ent_Disponibles_Saldos(Int32 codigo_reporte,Int32 ano, Decimal cantidad_dias,Int32 cedula)
+        {
+            Codigo_Reporte = codigo_reporte;
+            Ano = ano;
+            Cantidad_Dias = cantidad_dias;
+            Cedula = cedula;
+        }
+        #endregion
+    }
+}
---
+++ SIGVA-DEMO/Negocios/Neg_Saldos_Disponibles.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using AccesoDatos;
+
+namespace Negocios
+{
+   public class Neg_Saldos_Disponibles
+    {
+       #region "Eliminar"
+       public Int32 Eliminar(Int32 Cedula)
+       {
+           Int32 FilasAfectadas = 0;
+           AccesoDatos.AD_Saldos_Disponibles IControlador;
+
+           try
+           {
+               IControlador = new AccesoDatos.AD_Saldos_Disponibles();
+               FilasAfectadas = IControlador.Eliminar(Cedula);
+           }
+           catch (Exception ex)
+           {
+               throw new Exception(ex.Message, ex);
+           }
+
+           return FilasAfectadas;
+       }
+       #endregion
+    }
+}
